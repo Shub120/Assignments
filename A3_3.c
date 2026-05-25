8dfd255f79321ab918a1161f4a729d6040f4b26c
Shubham Bhapkar <sbhapkar703@gmail.com>
2026-05-26 01:09:21 +0530

    Add program to display even factors of a number
    Implement a program to display even factors of a user-provided number.

--- A3_3.c
+++ A3_3.c
@@ -1,2 +1,33 @@
 /*
-*/
+Write a Program which Accepts number from user and prints even factors from that number */
+
+#include <stdio.h>
+
+void DisplayEvenFactor(int iNo)
+{
+    int i=0;
+
+    if (iNo<=0)
+    {
+        iNo=-iNo;
+
+    }
+
+    for (i=1;i<=iNo/2;i++)
+    {
+        if (iNo % i==0 && i%2==0)
+        {
+           printf("%d ",i);
+        }
+    }
+}
+
+int main()
+{
+    int iValue=0;
+
+    printf("ENter the number : \n");
+    scanf("%d",&iValue);
+    
+    DisplayEvenFactor (iValue);
+}
