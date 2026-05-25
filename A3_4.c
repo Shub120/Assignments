cead47299279fba145aa4dc0d878bd08e215e4d8
Shubham Bhapkar <sbhapkar703@gmail.com>
2026-05-26 01:10:19 +0530

    Add files via upload

---
+++ A3_4.c
@@ -0,0 +1,19 @@
+/*
+Accept one character from the user and convert the case of that character */
+
+#include <stdio.h>
+
+void DisplayConvert (char CValue)
+{
+    if()
+}
+
+int main()
+{
+    char cValue ='\0';
+
+    printf("Enter the character : \n ");
+    scanf("%c",&cValue);
+
+    DisplayConvert(cValue);
+}
