b4803c2766e508dac3d639645a642405a35d1476
Shubham Bhapkar <sbhapkar703@gmail.com>
2026-05-26 11:33:37 +0530

    Add files via upload

--- A3_4.c
+++ A3_4.c
@@ -1,19 +1,34 @@
-/*
-Accept one character from the user and convert the case of that character */
+
+
+/* Accept one character from the user and convert the case of that character */
 
 #include <stdio.h>
 
 void DisplayConvert (char CValue)
 {
-    if()
+   
+    if(CValue >= 'a' && CValue <= 'z')
+    {
+        
+        CValue = CValue - 32; 
+        printf("%c",CValue);
+    }
+   
+    else if (CValue >= 'A' && CValue <= 'Z')
+    {
+       
+        
+        CValue = CValue + 32;
+        printf("%c",CValue);
+    }
 }
 
 int main()
 {
-    char cValue ='\0';
-
+    char cValue = '\0';
+    
     printf("Enter the character : \n ");
     scanf("%c",&cValue);
-
+    
     DisplayConvert(cValue);
 }
